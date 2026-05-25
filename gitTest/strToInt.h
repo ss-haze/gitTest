ecf24c99047b8953d2650db00948de35e981cddf
ss-haze <ss-haze@outlook.com>
2026-05-25 14:37:45 +0200

    created .h file

---
+++ gitTest/strToInt.h
@@ -0,0 +1,12 @@
+#ifndef STRTOINT
+#define STRTOINT
+
+int StrToInt(char str[]) {
+int intResult=0;
+for (int index=0; str[index]!='\0'; index++) {
+intResult=intResult * 10 + (str[index]-'0');
+}
+return intResult;
+}
+#endif
+
